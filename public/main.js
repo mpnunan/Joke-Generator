84875017e562364aed5073db224213023d23d092
Michael Nunan <michael.nunan@outlook.com>
2023-05-21 20:18:17 -0500

    in progress

--- public/main.js
+++ public/main.js
@@ -3,28 +3,35 @@
 import 'bootstrap'; // import bootstrap elements and js
 import '../styles/main.scss';
 import { jokeButton, punchlineButton, newJokeButton } from '../components/buttons';
+import { jokeOnDom, punchlineOnDom } from '../components/logic';
 
 const htmlStructure = () => {
   document.querySelector('#app').innerHTML = `
-    <div id="jokeBtn"></div>
+    <div id="jokeCard">
+      <div id="jokeTxt"></div>
+      <div id="jokeBtn"></div>
+    </div>
   `;
-  jokeButton();
 };
 
 const events = () => {
+  jokeButton();
   document.querySelector('#jokeBtn').addEventListener('click', (e) => {
     if (e.target.id.includes('the-joke')) {
       punchlineButton();
+      jokeOnDom();
     }
   });
   document.querySelector('#jokeBtn').addEventListener('click', (e) => {
     if (e.target.id.includes('the-punchline')) {
       newJokeButton();
+      punchlineOnDom();
     }
   });
   document.querySelector('#jokeBtn').addEventListener('click', (e) => {
     if (e.target.id.includes('another-joke')) {
-      jokeButton();
+      punchlineButton();
+      jokeOnDom();
     }
   });
 };
